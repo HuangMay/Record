2c97935f0937bff323ad9efa1a99f2ccd6f13a1d
HuangMay <may158182@gmail.com>
2015-07-27 14:10:20 +0800

    Initial Commit

---
+++ Record/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Record
+//
+//  Created by 傑夫 on 2015/7/27.
+//  Copyright (c) 2015年 傑夫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
